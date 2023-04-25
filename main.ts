55254739e3405fa4c2c5e9d2fc1ece03e44a4a40
emunfer1411 <130557119+emunfer1411@users.noreply.github.com>
2023-04-25 13:42:12 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,16 @@
 let velocidad = 40
+let contador = 0
 basic.forever(function () {
     if (maqueen.Ultrasonic(PingUnit.Centimeters) > 10) {
         velocidad = 50
         if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
             maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
+            for (let index = 0; index < 4; index++) {
+                maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+                maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+                maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+                maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+            }
         }
         if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0) {
             maqueen.motorStop(maqueen.Motors.M1)
@@ -14,8 +21,18 @@ basic.forever(function () {
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, velocidad)
         }
     } else {
+        music.playTone(262, music.beat(BeatFraction.Whole))
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
         maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
         maqueen.motorStop(maqueen.Motors.All)
     }
+    if (contador == 500) {
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    } else if (contador == 1000) {
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+        contador = 0
+    }
+    contador = contador + 1
 })
