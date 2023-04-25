888b8aa9090f638400298a655688764ae31db574
emunfer1411 <130557119+emunfer1411@users.noreply.github.com>
2023-04-25 13:19:08 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,16 +2,16 @@ let velocidad = 50
 basic.forever(function () {
     if (maqueen.Ultrasonic(PingUnit.Centimeters) > 10) {
         velocidad = 50
-        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
         }
         if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0) {
             maqueen.motorStop(maqueen.Motors.M1)
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, velocidad)
         }
         if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0) {
-            maqueen.motorStop(maqueen.Motors.M1)
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+            maqueen.motorStop(maqueen.Motors.M2)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, velocidad)
         }
     } else {
         maqueen.motorStop(maqueen.Motors.All)
