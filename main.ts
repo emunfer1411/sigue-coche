06d9f5ede9c4fb84e9427895cf2e9b3acc6127eb
emunfer1411 <130557119+emunfer1411@users.noreply.github.com>
2023-04-25 13:26:17 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,7 +9,7 @@ basic.forever(function () {
             maqueen.motorStop(maqueen.Motors.M1)
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, velocidad)
         }
-        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0) {
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
             maqueen.motorStop(maqueen.Motors.M2)
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, velocidad)
         }
